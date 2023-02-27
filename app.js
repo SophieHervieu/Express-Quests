59ac5b79334c5aadb7f58bb2db02f1d7cb01c5f6
SophieHervieu <[sophie-hervieu@live.fr](mailto:sophie-hervieu@live.fr)>
2023-02-27 17:23:43 +0100

    protected routes with authentication tokens

--- app.js
+++ app.js
@@ -16,14 +16,25 @@ app.get("/", welcome)
 const movieHandlers = require("./movieHandlers")
 
 const { validateMovie, validateUser } = require("./validators.js")
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js")
 
 app.get("/api/movies", movieHandlers.getMovies)
 app.get("/api/movies/:id", movieHandlers.getMovieById)
 app.get("/api/users", movieHandlers.getUsers)
 app.get("/api/users/:id", movieHandlers.getUsersById)
 
-app.post("/api/movies", movieHandlers.postMovie)
 // app.post("/api/users", movieHandlers.postUser)
+app.post("/api/users", validateUser, hashPassword, movieHandlers.postUser)
+
+app.post(
+  "/api/login",
+  movieHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+)
+
+app.use(verifyToken)
+
+// app.post("/api/movies", movieHandlers.postMovie)
 app.post("/api/movies", validateMovie, movieHandlers.postMovie)
 // app.post("/api/movies", validateUser, movieHandlers.postUser)
 
@@ -35,10 +46,20 @@ app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie)
 app.delete("/api/movies/:id", movieHandlers.deleteMovie)
 app.delete("/api/users/:id", movieHandlers.deleteUser)
 
-const { hashPassword } = require("./auth.js")
-
-app.post("/api/users", validateUser, hashPassword, movieHandlers.postUser)
 app.put("/api/users/:id", validateUser, hashPassword, movieHandlers.updateUser)
+
+// const isItDwight = (req, res) => {
+//   if (
+//     req.body.email === "dwight@theoffice.com" &&
+//     req.body.password === "123456"
+//   ) {
+//     res.send("Credentials are valid")
+//   } else {
+//     res.sendStatus(401)
+//   }
+// }
+
+app.post("/api/login")
 
 app.listen(port, (err) => {
   if (err) {
